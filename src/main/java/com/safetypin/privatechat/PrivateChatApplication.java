1a16e7ef6ee60a68d910186aa4db3e01e47e860c
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 19:47:48 +0700

    add: Hello World

--- src/main/java/com/safetypin/privatechat/PrivateChatApplication.java
+++ src/main/java/com/safetypin/privatechat/PrivateChatApplication.java
@@ -2,6 +2,9 @@ package com.safetypin.privatechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PrivateChatApplication {
@@ -10,4 +13,13 @@ public class PrivateChatApplication {
 		SpringApplication.run(PrivateChatApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+	@GetMapping
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
