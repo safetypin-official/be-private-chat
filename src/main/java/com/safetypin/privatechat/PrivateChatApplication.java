6fd7b3056cb9546c7b73d5184940b9aedbf42a3f
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 19:46:58 +0700

    Initial commit

---
+++ src/main/java/com/safetypin/privatechat/PrivateChatApplication.java
@@ -0,0 +1,13 @@
+package com.safetypin.privatechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatApplication.class, args);
+	}
+
+}
